81c1cf992c538b98c0d3f136988e7329cdfd11a3
Robson Paulo da Silva <robson_p_s@yahoo.com.br>
2025-09-20 16:10:45 -0300

    Last update

---
+++ src/app/product_detail/layout.tsx
@@ -0,0 +1,6 @@
+
+export default function ProductDetailLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <div id="main" className="flex justify-center py-6 bg-[#F7FAFC]">{children}</div>
+  );
+}
