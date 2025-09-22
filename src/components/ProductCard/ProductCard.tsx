601ef138ea2015eb7c7e310611b7cd13fe8a3fed
Robson Paulo da Silva <robson_p_s@yahoo.com.br>
2025-09-22 18:55:39 -0300

    modified:   src/components/ProductCard/ProductCard.tsx

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -1,10 +1,7 @@
 // components/ProductCard.tsx
-
 import Image from 'next/image';
 import Link from 'next/link';
 
-// Definimos o tipo das propriedades (props) que o componente irá receber,
-// tornando 'url_external' opcional com o `?`
 interface ProductCardProps {
   imageUrl: string;
   description: string;
@@ -16,29 +13,30 @@ export default function ProductCard({
   imageUrl,
   description,
   price,
-  url_external = '#' // Definimos o valor padrão aqui
+  url_external = '#',
 }: ProductCardProps) {
-
-  // A partir do Next.js 13, as props são aplicadas diretamente no Link.
   return (
-    <>
+    <Link
+      href={url_external}
+      className="block w-full h-full bg-white rounded-lg shadow hover:shadow-md overflow-hidden"
+    >
       {/* Imagem do Produto */}
       <div className="relative w-full h-48">
         <Image
           src={imageUrl}
-          alt={description} // O texto alternativo é crucial para acessibilidade
-          layout="fill"
-          objectFit="cover"
+          alt={description}
+          fill
+          className="object-cover"
         />
       </div>
 
       {/* Conteúdo do Cartão */}
-      <div className="p-4 flex flex-col justify-between flex-grow">
-        <p className="text-gray-700 text-base mb-2 flex-grow">{description}</p>
+      <div className="p-4 flex flex-col gap-2">
+        <p className="text-gray-700 text-base">{description}</p>
         <p className="text-xl font-bold text-gray-900 mt-auto">
-          ${price.toFixed(2)}
+          R$ {price.toFixed(2)}
         </p>
       </div>
-    </>
+    </Link>
   );
 }
